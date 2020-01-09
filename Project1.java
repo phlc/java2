d0914cc90ed0dedfd39f857b24f68e92b84f460c
phlc <pedrocarvalho@outlook.com>
2020-01-09 11:25:18 -0300

    Tests in Project1

--- Project1.java
+++ Project1.java
@@ -1,3 +1,16 @@
+import java.util.Arrays;
+
 public class Project1{
 
+  public static void main(String[] args){
+    int[] test = {1, 2, 3};
+    System.out.println(test);
+    System.out.println(Arrays.toString(test));
+
+    int[] test2 = test;
+    test2[1] = 4;
+    for (int i=0; i<test.length; i++){
+      System.out.println(test[i]);
+    }
+  }
 }
