83e76ce842d5efc95e6293e7573e96c88c28b3f8
phlc <pedrocarvalho@outlook.com>
2020-01-09 12:44:08 -0300

    Project1 contents to Tests. New Project1

--- Project1.java
+++ Project1.java
@@ -3,20 +3,6 @@ import java.util.Arrays;
 public class Project1{
 
   public static void main(String[] args){
-    int[] test = {1, 2, 3};
-    System.out.println(test);
-    System.out.println(Arrays.toString(test));
 
-    int[] test2 = test;
-    test2[1] = 4;
-    for (int i=0; i<test.length; i++){
-      System.out.println(test[i]);
-    }
-
-    int[][] test3 = {{1,2}, {2,3}};
-    System.out.println(Arrays.toString(test3));
-    for (int[] row: test3){
-      System.out.println(Arrays.toString(row));
-    }
-  }
-}
+  }//end of main Function
+}//end of class Project1
